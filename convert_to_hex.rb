f34ca0a16b8b014ce863db4f23b2bf1708a051b1
Lance Lacoste <lance.lacoste@gmail.com>
2016-03-31 22:04:40 -0500

    commit before changes

--- convert_to_hex.rb
+++ convert_to_hex.rb
@@ -1,9 +1,24 @@
 require 'RMagick'
 include Magick
+require 'ruby-progressbar'
 
-$input  = File.expand_path(ARGV[0])
-$output = File.expand_path(ARGV[1])
-$scale  = (ARGV[2].to_f || nil) rescue nil
+# CLI ARGS
+$input      = File.expand_path(ARGV[0])
+$output     = File.expand_path(ARGV[1])
+$scale      = (ARGV[2].to_f || nil) rescue nil
+
+# Pixel options
+$pixel_size       = 110
+$background_color = 'white'
+$font             = File.expand_path('Anonymous.ttf')
+$font_size        = 41
+
+
+puts "READING: #{$input}"
+puts "WRITING: #{$output}"
+puts "SCALE: #{$scale}"
+
+$progress = ProgressBar.create( :format => '%a %bᗧ%i %p%% %t', :progress_mark => ' ', :remainder_mark => '･', :starting_at => 0)
 
 def rgb(r, g, b)
   "##{to_hex r}#{to_hex g}#{to_hex b}"
@@ -13,28 +28,43 @@ def to_hex(n)
 end
 
 def generate_hex_pixel(pixel_color)
-  canvas = Magick::Image.new(110, 110){self.background_color = 'white'}
-  draw = Magick::Draw.new
-  draw.pointsize(41)
-  draw.font_weight = BoldWeight
-  draw.fill = pixel_color
-  draw.font = File.expand_path('Anonymous.ttf')
+  canvas = Magick::Image.new($pixel_size, $pixel_size){
+    self.background_color = $background_color
+  }
+  draw              = Magick::Draw.new
+  draw.font         = $font
+  draw.font_weight  = BoldWeight
+  draw.fill         = pixel_color
+  draw.pointsize($font_size)
 
-  draw.text( 5,39, "0".center(0))
-  draw.text(43,39, "0".center(0))
-  draw.text(80,39, "0".center(0))
 
-  draw.text( 5,72, pixel_color[1].center(0))
-  draw.text(43,72, pixel_color[2].center(0))
-  draw.text(80,72, pixel_color[3].center(0))
+  text                 = "000#{pixel_color[1..-1]}"
+  horizontal_positions = [39, 72, 105]
+  vertical_positions   = [5, 43, 80]
+  inc                  = 0
 
-  draw.text( 5,105, pixel_color[4].center(0))
-  draw.text(43,105, pixel_color[5].center(0))
-  draw.text(80,105, pixel_color[6].center(0))
+  horizontal_positions.each do |h_pos|
+    vertical_positions.each do |v_pos|
+      draw.text( v_pos, h_pos, text[inc].center(0))
+      inc += 1
+    end
+  end
+
+  # draw.text( 5,39, "0".center(0))
+  # draw.text(43,39, "0".center(0))
+  # draw.text(80,39, "0".center(0))
+  #
+  # draw.text( 5,72, pixel_color[1].center(0))
+  # draw.text(43,72, pixel_color[2].center(0))
+  # draw.text(80,72, pixel_color[3].center(0))
+  #
+  # draw.text( 5,105, pixel_color[4].center(0))
+  # draw.text(43,105, pixel_color[5].center(0))
+  # draw.text(80,105, pixel_color[6].center(0))
 
   draw.draw(canvas)
   canvas = canvas.rotate!(270).flip
-  canvas = canvas.scale(scale) if $scale
+  canvas = canvas.scale($scale) if $scale
   canvas
 end
 
@@ -44,6 +74,9 @@ pixels = img.get_pixels(0,0,img.columns,img.rows)
 convert = ImageList.new
 page    = Magick::Rectangle.new(0,0,0,0)
 
+total_progress = 0.0
+total          = (img.rows * img.columns).to_f
+
 for i in 0..img.rows
   for j in 0..img.columns
     pixel_color = img.pixel_color(i,j)
@@ -51,10 +84,18 @@ for i in 0..img.rows
     convert << tmp #.scale(0.25)
     page.x = j * tmp.columns
     page.y = i * tmp.rows
-    puts page
+    # puts page
     tmp.page = page
+    if (((total_progress / total.to_f) * 100).floor <= 99)
+      $progress.progress = ((total_progress / total.to_f) * 100).floor
+      total_progress += 1
+    else
+      $progress.finish
+    end
   end
 end
+
+puts 'Writing image, this could take a while...'
 
 mosaic = convert.mosaic.rotate!(90).flop
 mosaic.write($output)
